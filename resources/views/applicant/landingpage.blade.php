206283a32c930893f4666e78efee5ea4229566d5
bernie992 <102883005+bernie992@users.noreply.github.com>
2022-06-14 14:20:10 +0800

    june13 dayanan

--- resources/views/applicant/landingpage.blade.php
+++ resources/views/applicant/landingpage.blade.php
@@ -41,55 +41,10 @@
           
                                <!-- Button trigger modal -->
 
-                               <button type="button" class="btn btn-outline-primary btn-lg" data-toggle="modal" data-target="#exampleModal">
-                                Apply Job!
-                                </button>
-                               <a href="{{ url('/joblist') }}" class="btn btn-primary apply-btn">Apply Job</a>
+    
+                               <a style="background:#7FFD4;" href="{{ url('/joblist') }}" class="btn btn-primary apply-btn">Apply Job</a>
 
-                                <!-- Modal -->
-                                <div class="modal fade" id="exampleModal" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true">
-                                <div class="modal-dialog" role="document">
-                                    <div class="modal-content" >
-                                    <div class="modal-header">
-                                        <h5 class="modal-title" id="exampleModalLabel">Applicant Form</h5>
-                                        <button type="button" class="close" data-dismiss="modal" aria-label="Close">
-                                        <span aria-hidden="true">&times;</span>
-                                        </button>
-                                    </div>
-                                    <div class="modal-body">
-                                    <form>
-                                <div class="form-group">
-                                    <label>Name</label>
-                                    <input class="form-control" type="text">
-                                </div>
-                                <div class="form-group">
-                                    <label>Email Address</label>
-                                    <input class="form-control" type="text">
-                                </div>
-                                <div class="form-group">
-                                    <label>Message</label>
-                                    <textarea class="form-control"></textarea>
-                                </div>
-                                <div class="form-group">
-                                    <label>Upload your CSV</label></button2>
-                                    <div class="custom-file">
-                                        <input type="file" class="custom-file-input" id="cv_upload">
-                                        <button1 class="button button1" 8px> <label class="custom-file-label" for="cv_upload">Choose file</label></button1><input class="border" type="text">
-                                    </div>
-                                </div>
-                                <div class="submit-section">
-                                    <button class="btn btn-primary submit-btn">Submit</button>
-                                </div>
-                            </form>
-                                    
-                                    </div>
-                                </div>
-                                </div>
-                            </div>
-                        </div>
-                    </div>
-          
-                </div>
+                              
             </div>
         </div>
     </main>

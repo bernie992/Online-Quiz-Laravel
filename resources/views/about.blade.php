3a678d85b9fa7c0b7c3424b7d2623a6b05ac1bf6
bernie992 <102883005+bernie992@users.noreply.github.com>
2022-06-14 11:48:32 +0800

    june 13 dayanan

--- resources/views/about.blade.php
+++ resources/views/about.blade.php
@@ -15,7 +15,7 @@
     text-align: center;
     font-family: italic;
     font-weight: bold;
-    color: olive;
+    color: black;
   }
   .image{
       position: absolute;
@@ -37,7 +37,7 @@
     <div class="container">
 <h3>Lorem Ipsum</h3>
         <div class="row">
-                    <p style="color: olive;">What is Lorem Ipsum?
+                    <p style="color: black;">What is Lorem Ipsum?
                     <br>
                     Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.
                     <br> <br>
